9e9b2c9d418f60b1a3e5f4ec6cf6059691edbc03
vishnu s <vishnusudeesh390@gmail.com>
2024-01-11 01:09:07 +0530

    search

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+
+
+const font = Poppins({
+    subsets:["latin"],
+    weight:["200","300","400","600","500","700","800"]
+});
+
+
+export const Logo = ()=>{
+    return(
+         <Link href="/">
+            <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+                <div className="bg-white rounded-full p-1">
+                    <Image
+                    src="/spooky.svg"
+                    alt="gamehub"
+                    height="32"
+                    width="32"
+                    />
+                </div>
+                <div className={cn(font.className)}>
+                    <p className="text-lg font-semibold">gamehub</p>
+                    <p className=" text-sm text-muted-foreground">Lets play</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
